9d2efdf0fa102acd33895c1037b0412bc71ad7eb
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-10-16 15:30:54 +0300

    add auto swagger documentation for api

--- algebraic_predictor/backend/settings.py
+++ algebraic_predictor/backend/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #local
+    # local
     'core',
+
+    # third-party
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
