a8f29c938f48d2e29928123e36125aa3e713048d
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-10-16 19:45:30 +0300

    tests for user creation endpoint

---
+++ algebraic_predictor/user/tests/test_user_api.py
@@ -0,0 +1,80 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+USER_CREATE_URL = reverse("user:create")
+
+
+class CreateUserApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_with_only_necessary_fields_success(self):
+        data = {
+            "email": "test@example.com",
+            "password": "testpassword123",
+        }
+
+        response = self.client.post(USER_CREATE_URL, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response_data = str(response.content)
+        self.assertIn(data["email"], response_data)
+        self.assertNotIn(data["password"], response_data)
+        user = get_user_model().objects.filter(email=data["email"])
+        self.assertTrue(user.exists())
+
+    def test_create_user_with_all_fields_succeess(self):
+        data = {
+            "email": "test@example.com",
+            "password": "testpassword",
+            "first_name": "First",
+            "last_name": "Last",
+        }
+
+        response = self.client.post(USER_CREATE_URL, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response_data = str(response.content)
+        self.assertIn(data["email"], response_data)
+        self.assertIn(data["first_name"], response_data)
+        self.assertIn(data["last_name"], response_data)
+        self.assertNotIn(data["password"], response_data)
+
+        user = get_user_model().objects.filter(email=data["email"])
+        self.assertTrue(user.exists())
+
+    def test_create_user_invalid_credentials(self):
+        data = {
+            "email": "test",
+            "password": "testpassword",
+        }
+
+        response = self.client.post(USER_CREATE_URL, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        user = get_user_model().objects.filter(email=data["email"])
+        self.assertFalse(user.exists())
+
+    def test_create_user_password_too_short(self):
+        data = {
+            "email": "test@example.com",
+            "password": "short",
+        }
+
+        response = self.client.post(USER_CREATE_URL, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        user = get_user_model().objects.filter(email=data["email"])
+        self.assertFalse(user.exists())
+
+    def test_create_user_with_alrealy_taken_email(self):
+        get_user_model().objects.create_user(email='test@example.com', password='testpassword')
+
+        data = {
+            "email": "test@example.com",
+            "password": "testpassword",
+        }
+
+        response = self.client.post(USER_CREATE_URL, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
