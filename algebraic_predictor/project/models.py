ed754b4e87ec447e1d2a4955b615aea8b381d5d4
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-10-23 18:15:27 +0300

    add project model

--- algebraic_predictor/project/models.py
+++ algebraic_predictor/project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    source = models.TextField(blank=True, null=True)
+    algebraic_model = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Project #%s' % self.id
