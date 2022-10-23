e31ff6106b12828e8954a09a84b7ed8d65acdd1b
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-10-23 20:01:12 +0300

    url refactor

--- algebraic_predictor/backend/urls.py
+++ algebraic_predictor/backend/urls.py
@@ -8,8 +8,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('user/', include('user.urls')),
-    path('project/', include('project.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/', include('project.urls')),
 
     # documentation
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
