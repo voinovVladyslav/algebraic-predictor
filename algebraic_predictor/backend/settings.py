b53424e94419e90b6101333edd59f3ae901b30c6
Vladyslav Voinov <voinovvladyslav@gmail.com>
2022-11-06 15:56:38 +0200

    connect Redis

--- algebraic_predictor/backend/settings.py
+++ algebraic_predictor/backend/settings.py
@@ -100,6 +100,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_LOCATION', 'redis://127.0.0.1:6379/1'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
